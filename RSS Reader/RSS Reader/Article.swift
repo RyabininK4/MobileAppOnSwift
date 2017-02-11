b8c91f8c60763c179481f96432292f1ca3136f37
Kirill Ryabinin <ryabinin_k@MacBook-Pro-Kirill.local>
2017-02-11 12:55:21 +0300

    App News reader

---
+++ RSS Reader/RSS Reader/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  RSS Reader
+//
+//  Created by Кирилл Рябинин on 08.02.17.
+//  Copyright © 2017 Кирилл Рябинин. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+
+    var headline: String?
+    var desc: String?
+    var author: String?
+    var url: String?
+    var imageUrl: String?
+    
+}
